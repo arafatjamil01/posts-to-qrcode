d29f25d6cde6b99d0dffd76fca26247dc91ee7c2
arafatjamil01 <arafatjamil01@gmail.com>
2024-12-14 15:36:50 +0600

    Added common callback function to showcase mutliple settings

--- posts_to_qrcode.php
+++ posts_to_qrcode.php
@@ -56,28 +56,57 @@ function ptqc_qr_code_dimension() {
  */
 add_action( 'admin_init', 'ptqc_settings_init' );
 
+/**
+ * Function to initialize the settings with admin_init hook.
+ *
+ * @return void
+ */
 function ptqc_settings_init() {
 	// Add settings sections.
 	add_settings_section( 'ptqc_section', __( 'QR Code Settings', 'post-to-qrcde' ), 'ptqc_section_callback', 'general' );
 
-	// Add settings field
-	add_settings_field( 'ptqc_height', __( 'QR Code Height', 'post-to-qrcde' ), 'ptqc_height_callback', 'general', 'ptqc_section' );
-	add_settings_field( 'ptqc_width', __( 'QR Code Width', 'post-to-qrcde' ), 'ptqc_width_callback', 'general', 'ptqc_section' );
+	// Add settings field.
+	add_settings_field( 'ptqc_height', __( 'QR Code Height', 'post-to-qrcde' ), 'ptqc_display_field', 'general', 'ptqc_section', array( 'ptqc_height' ) );
+	add_settings_field( 'ptqc_width', __( 'QR Code Width', 'post-to-qrcde' ), 'ptqc_display_field', 'general', 'ptqc_section', array( 'ptqc_width' ) );
+	add_settings_field( 'ptqc_extra', __( 'Extra', 'post-to-qrcde' ), 'ptqc_display_field', 'general', 'ptqc_section', array( 'ptqc_extra' ) );
 
 	// register the settings to get the value from the options table.
 	register_setting( 'general', 'ptqc_height', array( 'sanitize_callback' => 'esc_attr' ) );
 	register_setting( 'general', 'ptqc_width', array( 'sanitize_callback' => 'esc_attr' ) );
+	register_setting( 'general', 'ptqc_extra', array( 'sanitize_callback' => 'esc_attr' ) );
 }
 
+/**
+ * Function to show a section on a callback.
+ */
 function ptqc_section_callback() {
 	echo '<p>' . __( 'Custom settings for QR Code', 'post-to-qrcde' ) . '</p>';
 }
 
+/**
+ * Single callback for all the settings one. This function will be called automatically
+ * and the data will render naturally every time this data is called.
+ */
+function ptqc_display_field( $args ) {
+	$option = get_option( $args[0] ); // $args[0] is for the first argument, since the function add_settings_field passed only one argument, the first one will appear here.
+	printf( '<input type="text" id="%s" name="%s" value="%s" />', $args[0], $args[0], $option );
+}
+
+/**
+ * Function to display the height field form.
+ *
+ * @return void
+ */
 function ptqc_height_callback() {
 	$height = get_option( 'ptqc_height' );
 	printf( '<input type="text" id="ptqc_height" name="ptqc_height" value="%s" />', $height );
 }
 
+/**
+ * Function to display the width field form.
+ *
+ * @return void
+ */
 function ptqc_width_callback() {
 	$width = get_option( 'ptqc_width' );
 	printf( '<input type="text" id="ptqc_width" name="ptqc_width" value="%s" />', $width );
